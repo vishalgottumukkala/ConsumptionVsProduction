ca9277d67b9fb4f772d36c27ae2f8f62db780863
Vishal Gottumukkala <vishalgottumukkala@tamu.edu>
2020-08-13 15:25:27 -0500

    Ui

---
+++ Program/ui.R
@@ -0,0 +1,12 @@
+shinyServer(
+  pageWithSidebar(
+    headerPanel("Consumption of O2 Vs. Production of CO2"),
+    
+    sidebarPanel("Side Bar"),
+    
+    mainPanel("Main Panel")
+    
+  )
+  
+  
+  )
